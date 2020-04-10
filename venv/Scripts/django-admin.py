9042036ebdda6d31274c76987d7a21a703446852
wikiccu <privatemx.2010@gmail.com>
2020-04-10 19:38:03 +0430

    venv removed

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\saeed\documents\github\pydjango\python\myfirstproj\pshop\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
