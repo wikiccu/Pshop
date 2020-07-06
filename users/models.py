c20bf8e955ad6ba8cda60f9c1e560106848fd823
wikiccu <privatemx.2010@gmail.com>
2020-07-06 18:20:21 +0430

    last commit

--- users/models.py
+++ users/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
+
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
